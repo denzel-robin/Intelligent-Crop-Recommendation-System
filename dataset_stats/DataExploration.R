1ed9c9ef82d55b75cfaa400626dffecc9907cb1e
Adithza <adithza77@gmail.com>
2026-02-10 10:18:38 +0530

    Boxplots and changes

--- dataset_stats/DataExploration.R
+++ dataset_stats/DataExploration.R
@@ -1,14 +1,10 @@
-# Clear workspace
 rm(list = ls())
 
-# Load data
 data <- read.csv("Crop_recommendation.csv")
 
-# View data
 head(data)
 tail(data)
 
-# Check size and columns
 dim(data)
 names(data)
 str(data)
@@ -19,50 +15,41 @@ colSums(is.na(data))
 #duplicate values
 sum(duplicated(data))
 
-# Summary stats
 summary(data)
 
-# Numeric columns only
 num_cols <- sapply(data, is.numeric)
 numeric_data <- data[, num_cols]
 
-# Mean, SD, Min, Max
 stats <- data.frame(
-  Mean = sapply(numeric_data, mean),
-  SD = sapply(numeric_data, sd),
-  Variance = sapply(numeric_data, var),
-  Min = sapply(numeric_data, min),
-  Max = sapply(numeric_data, max)
+  Mean = sapply(numeric_data, mean, na.rm = TRUE),
+  SD = sapply(numeric_data, sd, na.rm = TRUE),
+  Variance = sapply(numeric_data, var, na.rm = TRUE),
+  Min = sapply(numeric_data, min, na.rm = TRUE),
+  Max = sapply(numeric_data, max, na.rm = TRUE)
 )
 
 print(stats)
 
-# Class distribution
+
 table(data$label)
 prop.table(table(data$label))
-
-# Histograms
-hist(data$temperature)
-hist(data$humidity)
-hist(data$rainfall)
-hist(data$ph)
 
 # Boxplots
 boxplot(data$rainfall)
 boxplot(data$temperature)
+boxplot(data$humidity)
+boxplot(data$ph)
 
 # Correlation matrix
-cor_matrix <- cor(numeric_data)
+cor_matrix <- cor(numeric_data, use = "complete.obs")
 print(cor_matrix)
 
 
 # Skewness
 library(e1071)
 
+sapply(numeric_data, skewness, na.rm = TRUE)
 
-sapply(numeric_data, skewness)
-
-# Save stats
 write.csv(stats, "summary_statistics.csv", row.names = TRUE)
 
 # End
